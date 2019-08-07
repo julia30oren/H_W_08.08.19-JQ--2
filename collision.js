6ffcd1a8af006783f9ebe58eb5acb621e721b0c9
julia30oren <juliaoren30@gmail.com>
2019-08-07 16:39:16 +0300

    start game...

--- collision.js
+++ collision.js
@@ -1,10 +1,14 @@
-
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
+
+let mouse = {
+    x: innerWidth/2,
+    y: innerHeight/2
+};
 
 const colors = [
     '#2185C5',
@@ -37,22 +41,21 @@ function rundomColor(colors) {
     return colors [Math.floor(Math.random()*colors.length)];
 };
 
+function getDistance(x1, y1, x2, y2) {
+    let xDistance = x2-x1;
+    let yDistance = y2-y1;
 
+    return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+}
 
-function Ball(x, y, dy, radius, color) {
+function Circle(x, y, radius, color) {
     this.x = x;
     this.y = y;
-    this.dy = dy;
     this.radius = radius;
     this.color = color;
 
     this.update = function(){
-        if (this.y + this.radius > canvas.height) {
-            this.dy = - this.dy * 0.9;
-        }else{
-            this.dy += 1;
-        }
-        this.y += this.dy/2;
+
         this.draw();
     };
 
@@ -61,34 +64,39 @@ function Ball(x, y, dy, radius, color) {
         c.arc(this.x, this.y, this.radius, 0,Math.PI * 2, false);
         c.fillStyle = this.color;
         c.fill();
-        c.stroke();
         c.closePath();
+
+        // console.log(getDistance(circle1.x, circle1.y, circle2.x, circle2.y))
     };
 };
 
-var ball;
-var ballArray = [];
-function init() {
-    var radius = 15;
-    for (i=0; i<200; i++) {
-        var x = rundomIntFromRange(0, canvas.width);
-        var y = rundomIntFromRange(0, canvas.height - radius);
 
-        ballArray.push (new Ball(x, y, 2, 15, 'blue'));
-    }
-    // ball = new Ball(canvas.width/2, canvas.height/2 , 2, 30, 'red');
+
+
+let circle1;
+let circle2;
+function init() {
+    circle1 = new Circle(300, 300, 100, 'black');
+    circle2 = new Circle(10, 10, 30, 'red');
 }
 
 
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, canvas.width, canvas.height);
-
-    for (i=0; i< ballArray.length; i++) {
-        ballArray[i].update();
-    }
     // c.fillText("HTML CANVAS BOILERPLATE", mouse.x, mouse.y);
-    // ball.update();
+    circle1.update();
+    circle2.x = mouse.x;
+    circle2.y = mouse.y;
+    circle2.update();
+
+    if (getDistance(circle1.x, circle1.y, circle2.x, circle2.y) < circle1.radius + circle2.radius) {
+        circle1.color = '#2185C5';
+    }else{
+        circle1.color = 'black';
+    }
+
+    // console.log(getDistance(circle1.x, circle1.y, circle2.x, circle2.y))
 }
 
 init();
